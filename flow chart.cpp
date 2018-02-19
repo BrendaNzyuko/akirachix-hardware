6e9652a90ef6e950c78ad2d5a67e95805af75fde
Brenda Nzyuko <mbutubrenda97@gmail.com>
2018-02-19 16:10:58 +0300

    flow lesson

---
+++ flow chart.cpp
@@ -0,0 +1,43 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int a ;
+    
+     int b ;
+     
+     cout <<"enter first  number";
+     
+    cin>>a;
+    
+    cout<<"enter second number";
+    cin>>b;
+  
+  if( a > b){
+      cout << "1st is greater 2nd";
+      
+  }
+  else if(a==b){
+      cout<<"1st is equal to 2nd";
+  }
+      
+      else{"second is greater";
+          
+      }
+     
+
+    return 0;
+}
+
+
+
